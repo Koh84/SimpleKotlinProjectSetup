f787486d99d9cd5b642a38a6547978b9c5ac45e8
Kelvin Koh <kelvin.koh@prometheanworld.com>
2021-07-10 11:49:07 +0100

    simple project with 1 function with unit test

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-
+/**
+ * The MainActivity class.
+ */
 class MainActivity : AppCompatActivity() {
     companion object {
         private val TAG = MainActivity::class.java.simpleName
@@ -12,10 +14,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var result = addition(1, 2)
-        Log.d(TAG, "result = ${result.toString()}")
+        var kelvinMath = KelvinMath()
+        var result = kelvinMath.addition(1, 2)
+        Log.d(TAG, "kelvin test result = ${result.toString()}")
+    }
+}
+
+/**
+ * The KelvinMath class. Used to perform basic mathematical operation.
+ */
+class KelvinMath {
+    companion object {
+        private val TAG = KelvinMath::class.java.simpleName
     }
 
+    /**
+     * Perform addition of two integer numbers.
+     */
     fun addition(a: Int, b: Int): Int{
         var c = a+b
         return c
---
+++ app/src/test/java/com/example/myapplication/MainActivityTest.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+import android.content.Context
+import org.junit.Test
+
+class MainActivityTest {
+
+    @Test
+    fun addition() {
+        val kelvinMath = KelvinMath()
+        var expected = 5
+        var result = kelvinMath.addition(2, 3)
+        assert(expected == result)
+    }
+}
